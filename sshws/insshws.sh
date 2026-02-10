3176e03818292bf0b5dd53c9a6922f4ff4f5908a
vpngacor <agungstore988@gmail.com>
2026-02-10 14:21:29 +0700

    Add files via upload

---
+++ sshws/insshws.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#installer Websocker tunneling 
+
+cd
+
+#Install Script Websocket-SSH Python
+#wget -O /usr/local/bin/ws-openssh bebejje
+wget -O /usr/local/bin/ws-dropbear -##
+wget -O /usr/local/bin/ws-stunnel 8-2728+383+
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/vpngacor/gacor/sshws/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+
+#izin permision
+#chmod +x /usr/local/bin/ws-openssh
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel 
+egebenenebsy
+
